0c9a17cfbaad8c392007cbd5578e4876d8a4002b
EimyCastillo214 <ceimy9211@gmail.com>
2025-05-19 21:54:00 -0600

    Update asistencias.py

--- asistencias.py
+++ asistencias.py
@@ -1,10 +1,3 @@
-# prompt: leer archivo BASE DE DATOS.csv
-
-import pandas as pd
-excel_file_path = "BASE DE DATOS.xlsx"
-df = pd.read_excel(excel_file_path)
-print(df.to_string())
-
 # prompt: muestrame las columnas del archivo BASE DE DATOS.csv
 
 df.columns
