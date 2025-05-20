f32d9d8f274aa6dec54f17faa120b750fb56e40c
EimyCastillo214 <ceimy9211@gmail.com>
2025-05-19 19:00:28 -0600

    Update asistencias.py

--- asistencias.py
+++ asistencias.py
@@ -1,13 +1,2 @@
-# prompt: Leer el archivo BASE PROGRAMA.csv ubicado en la carpeta MyDrive de la carpeta drive
+# prompt: Leer el archivo BASE PROGRAMA.csv 
 
-from google.colab import drive
-drive.mount('/content/drive')
-
-import pandas as pd
-
-# Try reading with latin-1 encoding
-try:
-    df = pd.read_csv('/content/drive/MyDrive/BASE PROGRAMA.csv', encoding='latin-1')
-except UnicodeDecodeError:
-    # If latin-1 fails, try cp1252 encoding
-    df = pd.read_csv('/content/drive/MyDrive/BASE PROGRAMA.csv', encoding='cp1252')
