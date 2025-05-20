dde86ad740874f868ac21119a6a9492254b5ebc7
EimyCastillo214 <ceimy9211@gmail.com>
2025-05-19 19:04:26 -0600

    Update asistencias.py

--- asistencias.py
+++ asistencias.py
@@ -1,2 +1,7 @@
-# prompt: Leer el archivo BASE PROGRAMA.csv 
+# prompt: leer archivo BASE DE DATOS.xlsx
 
+import pandas as pd
+
+file_path = 'BASE DE DATOS.csv'
+df = pd.read_excel(file_path)
+print(df.head())
